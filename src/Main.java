94123a6f868ecbabcd44c4919cd5e5ff3e205224
fief2 <147194545+fief2@users.noreply.github.com>
2024-11-01 13:05:09 -0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,42 +2,42 @@ import java.util.*;
 import java.io.*;
 interface Ingredient {
     String getName();
-    int getQuantity();
+    double getQuantity();
 }
 class SolidIngredient implements Ingredient {
     private String _name;
-    private int _quantity;
-    public SolidIngredient(String n, int q) {
+    private double _qtyInGrams;
+    public SolidIngredient(String n, double q) {
         _name = n;
-        _quantity = q;
+        _qtyInGrams = q;
     }
     public String getName() {
         return _name;
     }
-    public int getQuantity(){
-        return _quantity;
+    public double getQuantity(){
+        return _qtyInGrams;
     }
 }
 class LiquidIngredient implements Ingredient {
     private String _name;
-    private int _quantity;
-    public LiquidIngredient(String n, int q) {
+    private double _qtyInMl;
+    public LiquidIngredient(String n, double q) {
         _name = n;
-        _quantity = q;
+        _qtyInMl = q;
     }
     public String getName() {
         return _name;
     }
-    public int getQuantity(){
-        return _quantity;
+    public double getQuantity(){
+        return _qtyInMl;
     }
 }
-class Recipe<T extends Ingredient> {
+class Recipe<T extends Ingredient> { //extends indicates a constraint for the type variable/parameter. Only keyword that can be used.
     private String _name;
     private String _instructions;
     private ArrayList<T> _ingredients;
-    public Recipe(String name, String instructions, int size) {
-        _ingredients = new ArrayList<T>(size);
+    public Recipe(String name, String instructions) { //can add int size as parameter
+        _ingredients = new ArrayList<T>();
         _name = name;
         _instructions = instructions;
     }
@@ -46,17 +46,44 @@ class Recipe<T extends Ingredient> {
     }
     public void print() {
         if(_ingredients != null) {
-            for(int i=0;i< _ingredients.size();i++) {
-                System.out.println("Ingredients: " + _ingredients.get(i).getClass().getName());
+            for (T ingredient : _ingredients) {
+                System.out.println("Ingredients: " + ingredient.getClass().getName() + " " + ingredient.getName() + " " + ingredient.getQuantity());
             }
         }
     }
 }
 public class Main {
+    public static void menu(Recipe<Ingredient> recipe) { //could try catch for type error exception for nextInt()
+        System.out.println("1. to add ingredient to recipe");
+        System.out.println("2. to print recipe");
+        System.out.println("-1. to exit");
+        Scanner sc = new Scanner(System.in);
+        int select = sc.nextInt();
+        while (select != -1) { //move to individual functions, have menu return choice, switch cases in main
+            if (select == 1) {
+                System.out.println("For solid ingredient type 's' for liquid type 'l'");
+                //need to differentiate type with input scan.nextLine().charAt(0);
+                System.out.println("Enter the ingredient name and quantity separated with a space");
+                String nameIn = sc.next();
+                double qtyIn = sc.nextDouble(); //try catch
+                recipe.addIngredient(new SolidIngredient(nameIn, qtyIn));
+                //separate adding solid and liquid
+            }
+            if (select == 2) {
+                recipe.print();
+            }
+            System.out.println("1. to add ingredient to recipe");
+            System.out.println("2. to print recipe");
+            System.out.println("-1. to exit");
+            select = sc.nextInt();
+        }
+    }
+
     public static void main(String[] args) {
-        Recipe<SolidIngredient> solidRecipe= new Recipe<>("Sandwich", "Instructions", 2);
-        solidRecipe.addIngredient(new SolidIngredient("bread", 2));
-        solidRecipe.addIngredient(new SolidIngredient("cheese", 1));
-        solidRecipe.print();
+        Recipe<Ingredient> recipe = new Recipe<>("Sandwich", "Put cheese in bread");
+        //Ingredient in Recipe template works similarly to creating an ArrayList<Employees> from an abstract class type
+        //since each element is not instantiated only the polymorphic variables are declared as this type
+        //can similarly declare a variable with interface type (Ingredient ing;)
+        menu(recipe);
     }
 }
