628b5d0d15f7f87ab265c656f073116ab0322cac
fief2 <147194545+fief2@users.noreply.github.com>
2024-11-04 11:45:35 -0600

    second commit

--- src/Main.java
+++ src/Main.java
@@ -53,37 +53,57 @@ class Recipe<T extends Ingredient> { //extends indicates a constraint for the ty
     }
 }
 public class Main {
-    public static void menu(Recipe<Ingredient> recipe) { //could try catch for type error exception for nextInt()
+    public static void menu(Recipe<Ingredient> recipe, Scanner s) { //could try catch for type error exception for nextInt()
         System.out.println("1. to add ingredient to recipe");
         System.out.println("2. to print recipe");
         System.out.println("-1. to exit");
         Scanner sc = new Scanner(System.in);
-        int select = sc.nextInt();
-        while (select != -1) { //move to individual functions, have menu return choice, switch cases in main
-            if (select == 1) {
-                System.out.println("For solid ingredient type 's' for liquid type 'l'");
-                //need to differentiate type with input scan.nextLine().charAt(0);
-                System.out.println("Enter the ingredient name and quantity separated with a space");
-                String nameIn = sc.next();
-                double qtyIn = sc.nextDouble(); //try catch
-                recipe.addIngredient(new SolidIngredient(nameIn, qtyIn));
-                //separate adding solid and liquid
+        try {
+            int select = sc.nextInt();
+            char type = 0;
+            while (select != -1) { //move to individual functions, have menu return choice, switch cases in main
+                if (select == 1) {
+                    System.out.println("For solid ingredient type 's' for liquid type 'l'");
+                    type = sc.next().charAt(0);
+                    addIngredient(recipe, type, sc);
+                }
+                if (select == 2) {
+                    recipe.print();
+                }
+                System.out.println("1. to add ingredient to recipe");
+                System.out.println("2. to print recipe");
+                System.out.println("-1. to exit");
+                select = sc.nextInt();
             }
-            if (select == 2) {
-                recipe.print();
-            }
-            System.out.println("1. to add ingredient to recipe");
-            System.out.println("2. to print recipe");
-            System.out.println("-1. to exit");
-            select = sc.nextInt();
+        }
+        catch(Exception e) {
+            System.out.println("Problem reading input");
+            System.exit(-1);
         }
     }
-
+    public static void addIngredient(Recipe<Ingredient> recipe, char type, Scanner sc) {
+        System.out.println("Enter the ingredient name and quantity separated with a space");
+        try {
+            String nameIn = sc.next();
+            double qtyIn = sc.nextDouble();
+            if(type == 's') {
+                recipe.addIngredient(new SolidIngredient(nameIn, qtyIn));
+            }
+            else if(type == 'l') {
+                recipe.addIngredient(new LiquidIngredient(nameIn, qtyIn));
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Problem reading input");
+            System.exit(-1);
+        }
+    }
     public static void main(String[] args) {
         Recipe<Ingredient> recipe = new Recipe<>("Sandwich", "Put cheese in bread");
+        Scanner sc = new Scanner(System.in);
         //Ingredient in Recipe template works similarly to creating an ArrayList<Employees> from an abstract class type
         //since each element is not instantiated only the polymorphic variables are declared as this type
         //can similarly declare a variable with interface type (Ingredient ing;)
-        menu(recipe);
+        menu(recipe, sc);
     }
 }
